a7d22d5c9eb5d9ce924aab94c1956e0035d8ef4e
Nicolas Grimaldi <91294866+Nico3000-creator@users.noreply.github.com>
2023-08-15 17:56:45 -0300

    share

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("shareIcon").addEventListener("click", function() {
+    var linkIcons = document.querySelector(".link_icons");
+    linkIcons.classList.toggle("active");
+});
+ 
